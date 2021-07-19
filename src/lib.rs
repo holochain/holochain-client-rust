8becac8c3767f83eb4d9e566eaeb05ac06f4cd47
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-07-19 11:39:24 +0200

    Reexport all necessary types for api

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,12 @@ mod error;
 
 pub use admin_websocket::AdminWebsocket;
 pub use app_websocket::AppWebsocket;
+pub use error::{ConductorApiError, ConductorApiResult};
+pub use holochain_conductor_api::{
+    AdminRequest, AdminResponse, AppRequest, AppResponse, AppStatusFilter, InstalledAppInfo,
+    ZomeCall,
+};
+pub use holochain_types::{
+    app::{InstallAppBundlePayload, InstalledAppId},
+    dna::AgentPubKey,
+};
