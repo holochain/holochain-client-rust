2e55b67d1cae72a9d8095281e7296216b1a4caf8
Jost Schulte <jost.schulte@protonmail.com>
2022-09-21 21:34:11 -0500

    add holochain nix conf

---
+++ default.nix
@@ -0,0 +1,11 @@
+let
+  holonixPath = (import ./nix/sources.nix).holonix;
+  holonix = import (holonixPath) {
+    holochainVersionId = "v0_0_162";
+  };
+  nixpkgs = holonix.pkgs;
+in
+nixpkgs.mkShell {
+  inputsFrom = [ holonix.main ];
+  packages = with nixpkgs; [];
+}
