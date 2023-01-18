11d318e382b68293f1acdfa110d54aa1a00818a9
Jost Schulte <jost.schulte@protonmail.com>
2023-01-18 13:39:46 -0600

    upgrade nix-shell to rc.3

--- default.nix
+++ default.nix
@@ -1,13 +1,13 @@
 let
   holonixPath = (import ./nix/sources.nix).holonix; # points to the current state of the Holochain repository
   holonix = import (holonixPath) {
-    holochainVersionId = "v0_1_0-beta-rc_2"; # specifies the Holochain version
+    holochainVersionId = "v0_1_0-beta-rc_3"; # specifies the Holochain version
   };
   nixpkgs = holonix.pkgs;
 in nixpkgs.mkShell {
   inputsFrom = [ holonix.main ];
   packages = with nixpkgs; [
-    niv
+    # niv
     nodejs
   ];
 }
