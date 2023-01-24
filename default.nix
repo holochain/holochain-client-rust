a32d0c36e2920a6bf0c9fb6c3ca2f1a2b9489583
Jost Schulte <jost.schulte@protonmail.com>
2023-01-23 19:00:25 -0600

    upgrade to holochain rc.4

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 let
-  holonixPath = (import ./nix/sources.nix).holonix; # points to the current state of the Holochain repository
+  holonixPath = (import ./nix/sources.nix).holonix;
   holonix = import (holonixPath) {
-    holochainVersionId = "v0_1_0-beta-rc_3"; # specifies the Holochain version
+    holochainVersionId = "v0_1_0-beta-rc_4";
   };
   nixpkgs = holonix.pkgs;
 in nixpkgs.mkShell {
