210a6750c4cbac4aaf4aac975b9cfc8360e5d519
Jost Schulte <jost.schulte@protonmail.com>
2023-01-23 19:10:43 -0600

    ci(macos): reenable in matrix

--- default.nix
+++ default.nix
@@ -2,6 +2,12 @@ let
   holonixPath = (import ./nix/sources.nix).holonix;
   holonix = import (holonixPath) {
     holochainVersionId = "v0_1_0-beta-rc_4";
+    include = {
+      node = false;
+      happs = false;
+      scaffolding = false;
+      launcher = false;
+    };
   };
   nixpkgs = holonix.pkgs;
 in nixpkgs.mkShell {
