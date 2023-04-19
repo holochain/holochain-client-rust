08aaf6e9054215fe6d71202c674dae237d6ecbb6
Jost Schulte <jost.schulte@protonmail.com>
2023-04-19 14:46:37 -0600

    build: temporarily add sqlite to nix flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   inputs = {
-    holonix.url = "github:holochain/holochain";
+    holonix = {
+      url = "github:holochain/holochain";
+      inputs.versions.url = "github:holochain/holochain/?dir=versions/0_1";
+      inputs.holochain.url = "github:holochain/holochain/holochain-0.2.0-beta-rc.4";
+    };
     nixpkgs.follows = "holonix/nixpkgs";
   };
 
@@ -15,6 +19,7 @@
             inputsFrom = [ holonix.devShells.${system}.holonix ];
             packages = with pkgs; [
               # add further packages from nixpkgs
+              sqlite
             ];
           };
         };
