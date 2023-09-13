dcb39dbbe937302d8a23d5950d2e9ddccbe8b6dd
Jost Schulte <jost.schulte@protonmail.com>
2023-09-13 14:50:52 -0600

    build(nix): update flake

--- flake.nix
+++ flake.nix
@@ -1,10 +1,9 @@
 {
   inputs = {
     nixpkgs.follows = "holonix/nixpkgs";
-    versions.url = "github:holochain/holochain?dir=versions/0_2";
-    holonix.url = "github:holochain/holochain";
+    versions.url = "github:holochain/holochain/nix/fix-and-pin-lair-0.3.0-input?dir=versions/0_2";
+    holonix.url = "github:holochain/holochain/nix/fix-and-pin-lair-0.3.0-input";
     holonix.inputs.versions.follows = "versions";
-    holonix.inputs.holochain.url = "github:holochain/holochain/holochain-0.2.2-beta-rc.3";
   };
 
   outputs = inputs@{ holonix, ... }:
