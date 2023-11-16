f4d4800b4a0351b9d4d89383a85d1e0246cf2da1
Jost Schulte <jost.schulte@protonmail.com>
2023-11-16 11:54:44 -0600

    ci: run on push to develop

--- build-fixture.sh
+++ build-fixture.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+set -e
 
 cd fixture/zomes/foo
 cargo build --release --target wasm32-unknown-unknown --target-dir ./target
 cd ../.. # into fixtures
 hc dna pack . -o test.dna
 hc app pack . -o test.happ
-cd .. # into root folder
