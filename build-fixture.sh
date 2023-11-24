1b2df31369f1e6f51d7b0cafd91e57d9226c7b65
Jost Schulte <jost.schulte@protonmail.com>
2023-11-24 12:41:10 -0600

    ci: leave .cargo out of fixture cache

--- build-fixture.sh
+++ build-fixture.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
+set -e
 
-cd fixture/zomes/foo
-cargo build --release --target wasm32-unknown-unknown --target-dir ./target
-cd ../.. # into fixtures
-hc dna pack . -o test.dna
-hc app pack . -o test.happ
-cd .. # into root folder
+cargo build -p test_wasm_foo --release --target wasm32-unknown-unknown --target-dir ./fixture/zomes/foo/target
+hc dna pack ./fixture -o ./fixture/test.dna
+hc app pack ./fixture -o ./fixture/test.happ
