612293c2fb2f786144969c64e22e4a1204470b81
ThetaSinner <ThetaSinner@users.noreply.github.com>
2024-02-29 16:26:48 +0000

    feat(admin-ws): add export so the type can be used outside the lib (#46)

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ mod admin_websocket;
 mod app_websocket;
 mod error;
 
-pub use admin_websocket::AdminWebsocket;
+pub use admin_websocket::{AdminWebsocket, EnableAppResponse};
 pub use app_websocket::AppWebsocket;
 pub use error::{ConductorApiError, ConductorApiResult};
 pub use holochain_conductor_api::{
