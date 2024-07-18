3a613810a096f0a15e1155fa8c49b3b42dd878b5
Jost Schulte <jost.schulte@protonmail.com>
2024-07-18 07:45:49 +0200

    build(nix): add go

--- flake.nix
+++ flake.nix
@@ -16,6 +16,7 @@
               inputs'.holonix.packages.holochain
               inputs'.holonix.packages.lair-keystore
               inputs'.holonix.packages.rust
+              pkgs.go
             ] ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
               # needed to build Holochain on macos
               pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
