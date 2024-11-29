b84b0623c35c0a7c775c2087662686f4455a263f
Jost Schulte <jost-s@users.noreply.github.com>
2024-11-29 12:44:00 -0600

    build(nix): update flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    holonix.url = "github:holochain/holonix?ref=main-0.4";
+    holonix.url = "github:holochain/holonix?ref=main";
     nixpkgs.follows = "holonix/nixpkgs";
   };
 
@@ -17,14 +17,12 @@
               inputs'.holonix.packages.lair-keystore
               inputs'.holonix.packages.rust
               pkgs.go
+              pkgs.perl
             ] ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
               # needed to build Holochain on macos
               pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+              pkgs.bzip2
             ]);
-
-            shellHook = ''
-              export PS1='\[\033[1;34m\][holonix:\w]\$\[\033[0m\] '
-            '';
           };
         };
     };
