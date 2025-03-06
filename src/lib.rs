fc2d8fc2d10973b66cc6df38efc49b42f13120c7
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-03-06 16:32:36 +0000

    feat: Expose more types from Holochain libraries (#155)
    * feat: Expose more types from Holochain libraries
    
    * chore: Export more types
    
    * docs: Export more types

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,8 @@ pub use app_websocket::{AppWebsocket, ZomeCallTarget};
 pub use error::{ConductorApiError, ConductorApiResult};
 pub use holochain_conductor_api::{
     AdminRequest, AdminResponse, AppAuthenticationRequest, AppAuthenticationToken,
-    AppAuthenticationTokenIssued, AppInfo, AppRequest, AppResponse, AppStatusFilter,
-    IssueAppAuthenticationTokenPayload,
+    AppAuthenticationTokenIssued, AppInfo, AppRequest, AppResponse, AppStatusFilter, CellInfo,
+    IssueAppAuthenticationTokenPayload, ProvisionedCell,
 };
 pub use holochain_types::{
     app::{InstallAppPayload, InstalledAppId},
@@ -19,6 +19,9 @@ pub use holochain_types::{
     websocket::AllowedOrigins,
 };
 pub use holochain_websocket::{ConnectRequest, WebsocketConfig};
+pub use holochain_zome_types::prelude::{
+    CellId, ClonedCell, ExternIO, GrantedFunctions, SerializedBytes, Timestamp,
+};
 pub use signing::client_signing::{ClientAgentSigner, SigningCredentials};
 #[cfg(feature = "lair_signing")]
 pub use signing::lair_signing::LairAgentSigner;
